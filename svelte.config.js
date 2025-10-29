1fac17aa5a323f44850fb3c2480331de21e17efc
an-k45 <an_k27@protonmail.com>
2025-10-29 15:35:25 -0400

    build again

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,19 @@
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 
-const dev = "production" === "development";
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-		    pages: "docs",
-		    assets: "docs"
-		}),
-		prerender: {
-			entries: ['*'] // prerender everything
-		},
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/urban-air-quality' : ''
-		}		  
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/urban-air-quality' : ''
+    },
+    prerender: {
+      handleHttpError: 'warn'
+    }
+  }
 };
 
 export default config;
