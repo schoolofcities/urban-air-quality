0d9a7e55c796fe6f2e658f7b0eca5046f9d45e36
an-k45 <an_k27@protonmail.com>
2025-10-29 15:46:28 -0400

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = "production" === "development";
 
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'docs',
-      assets: 'docs',
-      fallback: 'index.html'
-    }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/urban-air-quality' : ''
-    },
-    prerender: {
-      handleHttpError: 'warn'
-    }
-  }
+
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+		    pages: "docs",
+		    assets: "docs"
+		}),
+		paths: {
+		    base: dev ? "" : "/urban-air-quality",
+		}
+	}
 };
 
 export default config;
